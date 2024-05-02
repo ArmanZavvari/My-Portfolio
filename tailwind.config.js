58765144745b4f8fbf17d3ad585d71c5b3bdd2c2
Arman Zavvari <armanzavvari@gmail.com>
2024-05-02 11:46:51 +0330

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,136 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    screens: {
+      xs: '370px',
+      // => @media (min-width: 370px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '860px',
+      // => @media (min-width: 860px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      keyframes: {
+        pulsate: {
+          '0%': {
+            transform: 'scale(1)',
+          },
+          '50%': {
+            transform: 'scale(1.05)',
+            'box-shadow': '0 10px 40px rgba(0, 0, 0, 0.25)',
+          },
+          '100%': {
+            transform: 'scale(1)',
+            'box-shadow': 'none',
+          },
+        },
+
+        moveInLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-100px)',
+          },
+          '80%': {
+            transform: 'translateX(10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+
+        moveInRight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(100px)',
+          },
+          '80%': {
+            transform: 'translateX(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translate(0)',
+          },
+        },
+
+        btnMoveInBottom: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(100px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+
+        imgMoveInRight: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(130px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+
+        heroMoveInBottom: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(100%)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+
+        openNavModal: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-100px)',
+          },
+          '100%': {
+            opacity: '100',
+            transform: 'translateY(0)',
+          },
+        },
+
+        closeNavModal: {
+          '0%': {
+            opacity: '100',
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(-100px)',
+          },
+        },
+      },
+
+      animation: {
+        pulsate: 'pulsate 1s infinite',
+        moveInLeft: 'moveInLeft 2s ease-out',
+        moveInRight: 'moveInRight 2s ease-out',
+        btnMoveInBottom: 'btnMoveInBottom 1.8s ease-out backwards',
+        imgMoveInRight: 'imgMoveInRight 2s ease-out',
+        heroMoveInBottom: 'heroMoveInBottom 1s ease-out',
+        openNavModal: 'openNavModal 0.4s ease-out forwards',
+        closeNavModal: 'closeNavModal 0.4s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+};
